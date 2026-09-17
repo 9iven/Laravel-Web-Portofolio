5aca7890a0503ba483d0c8736cb94bebb2799911
9iven <givengeraldchandra@mail.ugm.ac.id>
2026-09-17 10:39:56 +0700

    test mengganti footer ke pertemuan 5 dari 4

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,3 @@
 <footer class="border-t p-4 mt-8 text-center text-xs text-gray-500">
-    <p>&copy; {{ date('Y') }} Given Gerald Chandra &bull; PPWL Pertemuan 4</p>
+    <p>&copy; {{ date('Y') }} Given Gerald Chandra &bull; PPWL Pertemuan 5</p>
 </footer>
